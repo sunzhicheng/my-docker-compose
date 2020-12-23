4820f4868af3bec6ef1f8ee3377ab3be1ab7e791
walker <walker@idorp.com>
2020-12-23 14:11:54 +0800

    增加  phpamdin

--- cmd.sh
+++ cmd.sh
@@ -4,7 +4,7 @@ if [ -n "$1" ]; then
     CMD=$1
 else
     echo "指令用于进入服务环境"
-    echo "缺少参数支持服务有:db  redis openresty jenkins"
+    echo "缺少参数支持服务有:mysql  redis openresty jenkins"
     exit 1;
 fi
 docker-compose exec $CMD bash
